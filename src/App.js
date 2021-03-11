6f4bf2cb0d925e64959099008b603ba05ee449ae
unknown <jannathsyeda7@gmail.com>
2021-03-11 19:35:29 +0600

    practicng component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name="fahima";
+  var person={
+    name:"jannath",
+    id:"33",
+    section:"A",
+    proffession:"developer"
+
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <h1>hi, My name is {name}</h1>
+        <p>my id is {10+20}</p>
+
+        <p>another for object variable</p>
+        <h style={{backgroundColor:'tomato', color:'black', }}>ID:{person.id},  name: {person.name}</h>
+
+  
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +37,36 @@ function App() {
           Learn React
         </a>
       </header>
+
+ 
     </div>
   );
 }
+
+function Person(){
+
+  var person2={
+    name:"jannath",
+    id:"33",
+    section:"A",
+    proffession:"developer"
+
+  }
+
+const  personStyle={
+  border:"1px solid red",
+  boxShadow:"5px 5px 10px tomato" ,
+   margin:"30px",
+  width:"900px"
+  }
+  return(
+    <div style={personStyle}>
+      title: <h1>{person2.proffession}</h1>
+      <p>good to hear that..</p>
+    </div>
+
+  );
+}
+
 
 export default App;
